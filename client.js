f4a9e75d7062a6ca90ea645992e3b3a96a6a0c51
kilFront <kaifawebb@gmail.com>
2020-08-09 22:03:52 +0800

    update

--- client.js
+++ client.js
@@ -1,6 +1,4 @@
-const { resolve } = require("path");
-const { rejects } = require("assert");
-
+const net = require('net');
 class Request {
     constructor(options) {
         this.method = options.method || "GET";
@@ -21,10 +19,60 @@ class Request {
         this.headers["Content-Length"] = this.bodyText.length; // 必要的header，不推荐设计成可以从外面传 。写的length不对会是非法请求
     }
 
-    send() {
-        return new Promise((resolve, rejects) => {
-            //......
+    send(connection) {
+        return new Promise((resolve, reject) => {
+            // resolve("");
+            const parser = new ResponseParser;
+            if (connection) {
+                connection.write(this.toString());
+            } else {
+                connection = net.createConnection({
+                    host: this.host,
+                    port: this.port
+                }, () => {
+                    connection.write(this.toString());
+                })
+            }
+            connection.on('data', (data) => {
+                console.log(data.toString());
+                parser.receive(data.toString());
+                if (parser.isFinished) {
+                    resolve(parser.response);
+                    connection.end();
+                }
+            });
+            connection.on('error', (err) => {
+                console.log('err ---', err);
+                reject(err);
+                connection.end();
+            })
         })
+    }
+
+    toString() {
+            return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`
+            // return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r\n\r\n${this.bodyText}`
+        }
+        // toString() {
+        //     let stream = [
+        //         `${this.method} ${this.path} HTTP/1.1\r\n`,
+        //         ...Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}\r\n`),
+        //         '\r\n',
+        //         `${this.bodyText}\r\n`
+        //     ]
+        //     return stream.join('');
+        // }
+}
+
+class ResponseParser {
+    constructor() {}
+    receive(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.receiveChar(string.charAt(i));
+        }
+    }
+    receiveChar(char) {
+
     }
 }
 
